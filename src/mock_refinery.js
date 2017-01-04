d960e6090f7b8c4f24f3278e1ca514c975fc828a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-04 17:21:38 -0500

    Error handling; trim cruft

--- src/mock_refinery.js
+++ src/mock_refinery.js
@@ -2,13 +2,29 @@ define([],
     function () {
       return {
         parse_query: function () {
-          // TODO
-          return {
-            uuid: 'mock-uuid',
-            vis: 'refinery_heatmap_scatterplot'
-          }
+          // From http://stackoverflow.com/a/8486188
+          var query = location.search.substr(1);
+          var result = {};
+          query.split("&").forEach(function (part) {
+            if (!part) return;
+            part = part.split("+").join(" ");
+            var eq = part.indexOf("=");
+            var key = eq > -1 ? part.substr(0, eq) : part;
+            var val = eq > -1 ? decodeURIComponent(part.substr(eq + 1)) : "";
+            var from = key.indexOf("[");
+            if (from == -1) result[decodeURIComponent(key)] = val;
+            else {
+              var to = key.indexOf("]");
+              var index = decodeURIComponent(key.substring(from + 1, to));
+              key = decodeURIComponent(key.substring(0, from));
+              if (!result[key]) result[key] = [];
+              if (!index) result[key].push(val);
+              else result[key][index] = val;
+            }
+          });
+          return result;
         },
-        node: function (uuid) {
+        node: function (_uuid) {
           return new Promise(function (resolve, _reject) {
             var matrix = [['gene', 'cond-a', 'cond-b', 'cond-c', 'cond-d']];
             for (var i = 0; i < 40000; ++i) {
@@ -33,26 +49,10 @@ define([],
                       return row.join("\t")
                     }).join("\n"));
             resolve({
-              "uuid": "0dbf0d3d-6f11-4d14-ba7f-4f7c50e14420",
-              "name": "http://stemcellcommons.org/sites/default/files/chipseq/11610/GATA6%20ChIP-seq%20in%20differentiated%20cells%20+%20Caco2%20cell%20input%20DNA.bed",
-              "type": "Derived Data File",
-              "analysis_uuid": null,
-              "subanalysis": null,
-              "assay": "Measurement: transcription factor binding site identification; Technology: nucleotide sequencing; Platform: Illumina Genome Analyzer II; File: a_11610.txt",
-              "study": "11610: Transcription factor binding during intestinal cell differentiation [TFB]",
-              "relative_file_store_item_url": data_uri, // This is the only one that matters, for now.
-              "clustered_url": clustered_uri,
+              // This is the only one that matters, for now.
+              "relative_file_store_item_url": data_uri,
               // This doesn't necessarily come from the same API: Perhaps it is merged into the hash?
-              "parent_nodes": [
-                "ac0cdf9f-abb9-46b1-9b46-27c6be27be05"
-              ],
-              "child_nodes": [],
-              "auxiliary_nodes": [],
-              "is_auxiliary_node": false,
-              "file_extension": "bed",
-              "auxiliary_file_generation_task_state": null,
-              "ready_for_igv_detail_view": true,
-              "file_uuid": "528120a0-374b-42a4-977c-c25c0608c93b"
+              "clustered_url": clustered_uri
             })
           });
         }
--- src/refinery_heatmap_scatterplot.js
+++ src/refinery_heatmap_scatterplot.js
@@ -4,17 +4,27 @@
  the details of the calling environment, so each visualization
  should have a thin wrapper something like this:
  */
+requirejs.config({
+  "paths": {
+    "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/4.4.0/d3.min"
+  }
+});
 define(['refinery', 'd3', 'heatmap_scatterplot'],
     function (refinery, d3, heatmap_scatterplot) {
       return function () {
-        var uuid = refinery.parse_query(window.location.search).uuid;
-        refinery.node(uuid).then(function (node) {
+        var uuids = refinery.parse_query(window.location.search).uuids;
+        if (!uuids) {
+          throw new Error('"uuids" param is missing')
+        } else if (uuids.length != 1) {
+          throw new Error('Expected exactly one "uuids" param, not ' + uuids.length);
+        }
+        refinery.node(uuids[0]).then(function (node) {
           var url = node.relative_file_store_item_url;
           var clustered_url = node.clustered_url;
           var target = d3.select('#target');
           target.append('button')
               .text('Cluster')
-              .on('click', function() {
+              .on('click', function () {
                 d3.selectAll('#container *').remove();
                 render(clustered_url);
               });
